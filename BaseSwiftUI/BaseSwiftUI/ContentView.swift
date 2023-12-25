ae50cc2da75be01227a7ad82a66ba45cdbf928a1
dangtrunghieu1999 <hieudt230899@gmail.com>
2023-12-25 17:32:51 +0700

    complete layout with scrollview

--- BaseSwiftUI/BaseSwiftUI/ContentView.swift
+++ BaseSwiftUI/BaseSwiftUI/ContentView.swift
@@ -12,39 +12,80 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HeaderView()
-            HStack {
-                CardView(
-                    iconName: "character.book.closed.fill",
-                    title: "Books",
-                    description: "read, dream, belive, live")
+            ScrollView(showsIndicators: false) {
+                HStack {
+                    CardView(
+                        iconName: "character.book.closed.fill",
+                        title: "Books",
+                        description: "read, dream, belive, live")
+                    
+                    CardView(
+                        iconName: "music.quarternote.3",
+                        title: "Music",
+                        description: "listen, sing, dance"
+                    )
+                }
                 
-                CardView(
-                    iconName: "music.quarternote.3",
-                    title: "Music",
-                    description: "listen, sing, dance"
-                )
-            }
-            
-            ZStack {
-                CardView(
-                    iconName: "laptopcomputer.and.iphone",
-                    title: "Programing",
-                    description: "imagine, create, share, enjoy"
-                )
+                ZStack {
+                    CardView(
+                        iconName: "laptopcomputer.and.iphone",
+                        title: "Programing",
+                        description: "imagine, create, share, enjoy"
+                    )
+                    
+                    BadgeView(title: "SwiftUI",
+                              padding: 5,
+                              radius: 10,
+                              rotation: -35,
+                              xOffset: -125,
+                              yOffset: -70)
+                    
+                    BadgeView(title: "Layout using Stacks",
+                              padding: 10,
+                              radius: 20,
+                              rotation: 0,
+                              xOffset: 0,
+                              yOffset: 110)
+                }
                 
-                BadgeView(title: "SwiftUI",
-                          padding: 5,
-                          radius: 10,
-                          rotation: -35,
-                          xOffset: -125,
-                          yOffset: -70)
+                HStack {
+                    CardView(
+                        iconName: "character.book.closed.fill",
+                        title: "Books",
+                        description: "read, dream, belive, live")
+                    
+                    CardView(
+                        iconName: "music.quarternote.3",
+                        title: "Music",
+                        description: "listen, sing, dance"
+                    )
+                }
                 
-                BadgeView(title: "Layout using Stacks",
-                          padding: 10,
-                          radius: 20,
-                          rotation: 0,
-                          xOffset: 0,
-                          yOffset: 110)
+                HStack {
+                    CardView(
+                        iconName: "character.book.closed.fill",
+                        title: "Books",
+                        description: "read, dream, belive, live")
+                    
+                    CardView(
+                        iconName: "music.quarternote.3",
+                        title: "Music",
+                        description: "listen, sing, dance"
+                    )
+                }
+                
+                HStack {
+                    CardView(
+                        iconName: "character.book.closed.fill",
+                        title: "Books",
+                        description: "read, dream, belive, live")
+                    
+                    CardView(
+                        iconName: "music.quarternote.3",
+                        title: "Music",
+                        description: "listen, sing, dance"
+                    )
+                }
             }
             
             Spacer()
